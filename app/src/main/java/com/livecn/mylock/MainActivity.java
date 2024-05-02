9197ee604a3d38f705a0698994043294134ebfaa
live-cn <livecn@yahoo.com>
2024-05-02 22:59:27 +0800

    Update MainActivity.java

--- app/src/main/java/com/livecn/mylock/MainActivity.java
+++ app/src/main/java/com/livecn/mylock/MainActivity.java
@@ -1,4 +1,4 @@
-package com.livecn.mylock;
+package com.yourcompany.yourapp;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,7 +8,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private EditText pinField;
-    private final String correctPin = "757060";  // You should store and handle this securely
+    private final String correctPin = "1234";  // You should store and handle this securely
 
     @Override
     protected to onCreate(Bundle savedInstanceState) {
@@ -20,4 +20,15 @@ public class MainActivity extends Activity {
             if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                 // Check if the entered PIN is correct
                 if (pinField.getText().toString().equals(correctPin)) {
-                    Toast.makeText(MainActivity.this, "Unlocked!", Toast.LENGTH_SHORT).show
+                    Toast.makeText(MainActivity.this, "Unlocked!", Toast.LENGTH_SHORT).show();
+                    finish();  // Close the lock screen
+                } else {
+                    Toast.makeText(MainActivity.this, "Wrong PIN!", Toast.LENGTH_SHORT).show();
+                    pinField.setText("");
+                }
+                return true;
+            }
+            return false;
+        });
+    }
+}
